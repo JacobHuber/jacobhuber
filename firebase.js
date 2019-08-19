01e23025b9059fdc0847f3c27f20015c7507cce9
JacobHuber <xjacobhuber@gmail.com>
2019-08-19 00:34:22 -0600

    Privatized Read and Write

--- firebase.js
+++ firebase.js
@@ -13,3 +13,9 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
+
+firebase.auth().signInAnonymously().catch(function(error) {
+    // Handle Errors here.
+    //var errorCode = error.code;
+    //var errorMessage = error.message;
+});
