f25b707a4622dbf02a5354f94c20528c34f78d0b
JacobHuber <xjacobhuber@gmail.com>
2019-08-19 00:51:04 -0600

    Anonymous Authentication

--- firebase.js
+++ firebase.js
@@ -16,6 +16,30 @@ var database = firebase.database();
 
 firebase.auth().signInAnonymously().catch(function(error) {
     // Handle Errors here.
-    //var errorCode = error.code;
-    //var errorMessage = error.message;
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+});
+
+var uid = null;
+var loaded = false;
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // User is signed in.
+        // var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+
+        let stateCheck = setInterval(() => {
+            if (document.readyState === 'complete') {
+                clearInterval(stateCheck);
+
+                bodyLoad();
+            }
+        }, 100);
+    
+    } else {
+        // User is signed out.
+        // console.log("Not logged in.");
+    }
 });
